1d9fe873931adac0bf14a3ca9aacf395c3aca409
Hauke Neitzel <hauke.neitzel@googlemail.com>
2022-07-26 00:48:04 +0200

    implementing audio controller

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 mod emucore;
 
-use std::rc::Rc;
 use std::cell::RefCell;
+use std::rc::Rc;
 
 fn main() {
     println!("Hello, world!");
     let cartridge = emucore::mem::Cartridge::new(&[0]);
-    let mem = Rc::new(RefCell::new(emucore::mem::GBMemory::new(cartridge)));
+    let audio = Rc::new(RefCell::new(emucore::audio::SoundController::new()));
+    let mem = Rc::new(RefCell::new(emucore::mem::GBMemory::new(
+        cartridge,
+        audio.clone(),
+    )));
     let mut cpu = emucore::cpu::Cpu::new(mem);
     cpu.tick();
+    audio.borrow_mut().tick();
 }
